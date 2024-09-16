8ae09ca4896a715e9de1672c0b6af02fe58a0cf1
Francescosommellaa <franc.sommella@gmail.com>
2024-09-17 00:18:38 +0200

    created seeder and populated db with dummy data

---
+++ database/seeders/ComicSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Call ComicSeeder
+        $this->call(ComicSeeder::class);
+    }
+}
