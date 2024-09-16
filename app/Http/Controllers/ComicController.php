deacc9238b9f375262bed6e897a0347fe394ac42
Francescosommellaa <franc.sommella@gmail.com>
2024-09-17 00:11:07 +0200

    created the db and linked to the laravel project via the .env file, then migrated and created the controller and linked it to the web.php file.

---
+++ app/Http/Controllers/ComicController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comic;
+use Illuminate\Http\Request;
+
+class ComicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $comics = Comic::all();
+        return view('comics.index', compact('comics'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('comics.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'author' => 'required|string|max:255',
+            'publisher' => 'required|string|max:255',
+            'release_date' => 'required|date',
+            'price' => 'required|numeric',
+        ]);
+
+        $comic = Comic::create($data);
+        return redirect()->route('comics.show', $comic->id);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $comic = Comic::findOrFail($id);
+        return view('comics.show', compact('comic'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComicController;
 
+Route::resource('comics', ComicController::class);
 Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/about', [PageController::class, 'about'])->name('about');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
